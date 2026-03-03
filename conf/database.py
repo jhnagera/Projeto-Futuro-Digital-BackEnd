bd90bdfaa888d137709f000f384b864cd00e7b8e
jose henrique <grupo09tic55@proton.me>
2026-03-03 10:28:42 -0300

    alteração na database

--- conf/database.py
+++ conf/database.py
@@ -1,9 +1,16 @@
 from flask_sqlalchemy import SQLAlchemy
 
+load_dotenv()
 
 db = SQLAlchemy()
 
 def init_db(app):
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:123@localhost/projeto_escala'
+    database_url = os.getenv('DATABASE_URL')
+
+
+    if not database_url:
+        raise ValueError("DATABASE_URL não encontrado no arquivo .env")
+    
+    app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 
     db.init_app(app)
