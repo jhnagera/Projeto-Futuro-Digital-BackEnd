efed7b61604912f94a14623b1c2c4993d26fff38
jhnagera@gmail.com <grupo09tic55@proton.me>
2026-03-03 17:22:59 -0300

    backup do banco

--- conf/backup projeto escala.sql
+++ conf/backup projeto escala.sql
@@ -2,17 +2,14 @@
 -- PostgreSQL database dump
 --
 
-\restrict TQQyCLgANUX6kpboMLle5Vcc1AlLPUtciUPhuM0Br19bBDWchVTMhdmyGDWRT6b
-
 -- Dumped from database version 16.1
--- Dumped by pg_dump version 18.1
+-- Dumped by pg_dump version 16.1
 
--- Started on 2026-02-08 19:16:16
+-- Started on 2026-03-03 11:00:14
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
-SET transaction_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SELECT pg_catalog.set_config('search_path', '', false);
@@ -156,27 +153,27 @@ ALTER TABLE ONLY public.postos ALTER COLUMN id SET DEFAULT nextval('public.posto
 -- Data for Name: funcionarios; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO public.funcionarios (nome_completo, matricula, email, apelido, senha, horario_inicio, horario_fim, posto_especial) VALUES ('teste5', 200, 'te5@trt4.jus.br', 'Tchê', '123', NULL, '14:00', NULL);
-INSERT INTO public.funcionarios (nome_completo, matricula, email, apelido, senha, horario_inicio, horario_fim, posto_especial) VALUES ('José Henrique Luttjohann Nágera', 66621, 'jh@trt4.jus.br', 'Henrique', '12345', '13:00', '19:00', true);
 INSERT INTO public.funcionarios (nome_completo, matricula, email, apelido, senha, horario_inicio, horario_fim, posto_especial) VALUES ('Melissa', 98973, 'me@trt4.jus.br', 'Melissa', '123', '13:40', '19:40', true);
-INSERT INTO public.funcionarios (nome_completo, matricula, email, apelido, senha, horario_inicio, horario_fim, posto_especial) VALUES ('José Luis Menezes', 20320, 'jm@trt4.jus.br', 'Menezes', '123', '10:00', '14:00', false);
-INSERT INTO public.funcionarios (nome_completo, matricula, email, apelido, senha, horario_inicio, horario_fim, posto_especial) VALUES ('Luis Mirales', 65102, 'lm@trt4.jus.br', 'Mirales', '123', '12:00', '18:00', NULL);
 INSERT INTO public.funcionarios (nome_completo, matricula, email, apelido, senha, horario_inicio, horario_fim, posto_especial) VALUES ('Hamilton', 71811, 'h@trt4.jus.br', 'Hamilton', '123', '12:00', '18:00', NULL);
 INSERT INTO public.funcionarios (nome_completo, matricula, email, apelido, senha, horario_inicio, horario_fim, posto_especial) VALUES ('José Augusto', 67938, 'ja@trt4.jus.br', 'Augusto', '123', '14:00', '20:00', NULL);
-INSERT INTO public.funcionarios (nome_completo, matricula, email, apelido, senha, horario_inicio, horario_fim, posto_especial) VALUES ('Marcelo Pereira', 85731, 'mp@trt4.jus.br', 'Marcelo', '123', '13:00', '19:00', NULL);
-INSERT INTO public.funcionarios (nome_completo, matricula, email, apelido, senha, horario_inicio, horario_fim, posto_especial) VALUES ('Anderson Castalho', 89427, 'ac@trt4.jus.br', 'Castanho', '123', '7:00', '13:00', NULL);
-INSERT INTO public.funcionarios (nome_completo, matricula, email, apelido, senha, horario_inicio, horario_fim, posto_especial) VALUES ('Andre Melero', 97659, 'am@trt4.jus.br', 'Melero', '123', '7:00', '13:00', NULL);
-INSERT INTO public.funcionarios (nome_completo, matricula, email, apelido, senha, horario_inicio, horario_fim, posto_especial) VALUES ('Cristiano Lentz', 85600, 'cl@trt4.jus.br', 'Cristiano', '123', '7:00', '13:00', NULL);
-INSERT INTO public.funcionarios (nome_completo, matricula, email, apelido, senha, horario_inicio, horario_fim, posto_especial) VALUES ('Juliano S', 93530, 'js@trt4.jus.br', 'Juliano', '123', '7:00', '13:00', NULL);
-INSERT INTO public.funcionarios (nome_completo, matricula, email, apelido, senha, horario_inicio, horario_fim, posto_especial) VALUES ('Nereu de Oliveira', 33170, 'no@trt4.jus.br', 'Nereu', '123', '8:00', '14:00', NULL);
-INSERT INTO public.funcionarios (nome_completo, matricula, email, apelido, senha, horario_inicio, horario_fim, posto_especial) VALUES ('Claudenir dos Santos', 97128, 'cl@trt4.jus.br', 'Claudenir', '123', '8:00', '14:00', NULL);
-INSERT INTO public.funcionarios (nome_completo, matricula, email, apelido, senha, horario_inicio, horario_fim, posto_especial) VALUES ('Adriano D', 45365, 'ad@trt4.jus.br', 'Adriano', '123', '8:00', '14:00', NULL);
-INSERT INTO public.funcionarios (nome_completo, matricula, email, apelido, senha, horario_inicio, horario_fim, posto_especial) VALUES ('Rodrigo Maia', 96636, 'rm@trt4.jus.br', 'Maia', '123', '13:00', '19:00', NULL);
-INSERT INTO public.funcionarios (nome_completo, matricula, email, apelido, senha, horario_inicio, horario_fim, posto_especial) VALUES ('Manuel Melo', 97802, 'mm@trt4.jus.br', 'Manuel', '123', '12:00', '18:00', NULL);
-INSERT INTO public.funcionarios (nome_completo, matricula, email, apelido, senha, horario_inicio, horario_fim, posto_especial) VALUES ('Rafael Faustino', 122874, 'rf@trt4.jus.br', 'Faustino', '123', '13:00', '19:00', NULL);
 INSERT INTO public.funcionarios (nome_completo, matricula, email, apelido, senha, horario_inicio, horario_fim, posto_especial) VALUES ('Rodrigo Dossa', 96679, 'rd@trt4.jus.br', 'Dossa', '12345', '7:40', '13:40', true);
 INSERT INTO public.funcionarios (nome_completo, matricula, email, apelido, senha, horario_inicio, horario_fim, posto_especial) VALUES ('Eugenio B', 59986, 'eb@trt4.jus.br', 'Eugenio', '123', '6:40', '12:40', false);
 INSERT INTO public.funcionarios (nome_completo, matricula, email, apelido, senha, horario_inicio, horario_fim, posto_especial) VALUES ('Roberto V', 18910, 'rv@trt4.jus.br', 'Roberto', '123', '6:20', '12:20', true);
+INSERT INTO public.funcionarios (nome_completo, matricula, email, apelido, senha, horario_inicio, horario_fim, posto_especial) VALUES ('José Henrique Luttjohann Nágera', 66621, 'jh@trt4.jus.br', 'Henrique', '12345', '13:00', '19:00', true);
+INSERT INTO public.funcionarios (nome_completo, matricula, email, apelido, senha, horario_inicio, horario_fim, posto_especial) VALUES ('Marcio Bigolin', 66622, 'mb@trt4.jus.br', 'Bigolin', '123456', '13:00', '19:00', false);
+INSERT INTO public.funcionarios (nome_completo, matricula, email, apelido, senha, horario_inicio, horario_fim, posto_especial) VALUES ('Marcelo Pereira', 85731, 'mp@trt4.jus.br', 'Marcelo', '123', '07:10', '13:10', false);
+INSERT INTO public.funcionarios (nome_completo, matricula, email, apelido, senha, horario_inicio, horario_fim, posto_especial) VALUES ('Luis Mirales', 65102, 'lm@trt4.jus.br', 'Mirales', '123', '11:45', '17:45', false);
+INSERT INTO public.funcionarios (nome_completo, matricula, email, apelido, senha, horario_inicio, horario_fim, posto_especial) VALUES ('Anderson Castalho', 89427, 'ac@trt4.jus.br', 'Castanho', '123', '06:40', '12:40', false);
+INSERT INTO public.funcionarios (nome_completo, matricula, email, apelido, senha, horario_inicio, horario_fim, posto_especial) VALUES ('Andre Melero', 97659, 'am@trt4.jus.br', 'Melero', '123', '07:40', '13:40', false);
+INSERT INTO public.funcionarios (nome_completo, matricula, email, apelido, senha, horario_inicio, horario_fim, posto_especial) VALUES ('Cristiano Lentz', 85600, 'cl@trt4.jus.br', 'Cristiano', '123', '07:40', '13:40', false);
+INSERT INTO public.funcionarios (nome_completo, matricula, email, apelido, senha, horario_inicio, horario_fim, posto_especial) VALUES ('Nereu de Oliveira', 33170, 'no@trt4.jus.br', 'Nereu', '123', '07:40', '13:40', false);
+INSERT INTO public.funcionarios (nome_completo, matricula, email, apelido, senha, horario_inicio, horario_fim, posto_especial) VALUES ('Juliano S', 93530, 'js@trt4.jus.br', 'Juliano', '123', '06:40', '12:40', false);
+INSERT INTO public.funcionarios (nome_completo, matricula, email, apelido, senha, horario_inicio, horario_fim, posto_especial) VALUES ('Claudenir dos Santos', 97128, 'cl@trt4.jus.br', 'Claudenir', '123', '06:40', '14:10', false);
+INSERT INTO public.funcionarios (nome_completo, matricula, email, apelido, senha, horario_inicio, horario_fim, posto_especial) VALUES ('Adriano D', 45365, 'ad@trt4.jus.br', 'Adriano', '123', '06:40', '14:10', false);
+INSERT INTO public.funcionarios (nome_completo, matricula, email, apelido, senha, horario_inicio, horario_fim, posto_especial) VALUES ('Rodrigo Maia', 96636, 'rm@trt4.jus.br', 'Maia', '123', '13:10', '19:10', false);
+INSERT INTO public.funcionarios (nome_completo, matricula, email, apelido, senha, horario_inicio, horario_fim, posto_especial) VALUES ('Manuel Melo', 97802, 'mm@trt4.jus.br', 'Manuel', '123', '12:10', '18:10', false);
+INSERT INTO public.funcionarios (nome_completo, matricula, email, apelido, senha, horario_inicio, horario_fim, posto_especial) VALUES ('Rafael Faustino', 122874, 'rf@trt4.jus.br', 'Faustino', '123', '13:10', '19:10', true);
+INSERT INTO public.funcionarios (nome_completo, matricula, email, apelido, senha, horario_inicio, horario_fim, posto_especial) VALUES ('José Luis Menezes', 20320, 'jm@trt4.jus.br', 'Menezes', '123', '10:10', '16:10', false);
 
 
 --
@@ -188,13 +185,10 @@ INSERT INTO public.funcionarios (nome_completo, matricula, email, apelido, senha
 INSERT INTO public.postos (id, nome, descricao, posto_especial) VALUES (6, 'Ronda P1', 'Ronda em todo o Prédio 1', NULL);
 INSERT INTO public.postos (id, nome, descricao, posto_especial) VALUES (7, 'Delta 4', 'Sala cofre para acautelamento de objetos não permitidos', NULL);
 INSERT INTO public.postos (id, nome, descricao, posto_especial) VALUES (9, 'Galeria/QAP', NULL, NULL);
-INSERT INTO public.postos (id, nome, descricao, posto_especial) VALUES (13, 'Central', 'Gestão administrativa e atendimento ao publico ', NULL);
 INSERT INTO public.postos (id, nome, descricao, posto_especial) VALUES (8, 'Alfa 3', 'Entrada principal do Forum', NULL);
 INSERT INTO public.postos (id, nome, descricao, posto_especial) VALUES (10, 'Ronda P2 e P3', 'Ronda geral nos prédios 2 e 3', NULL);
 INSERT INTO public.postos (id, nome, descricao, posto_especial) VALUES (5, 'Alfa 2', 'Entrada de funcionários', NULL);
-INSERT INTO public.postos (id, nome, descricao, posto_especial) VALUES (14, 'Alfa 2iuhgikyg', 'Entrada de funcionários', NULL);
-INSERT INTO public.postos (id, nome, descricao, posto_especial) VALUES (16, 'teste4', 'teste4', NULL);
-INSERT INTO public.postos (id, nome, descricao, posto_especial) VALUES (18, 'Monitoramento', 'Monitorar as cameras', false);
+INSERT INTO public.postos (id, nome, descricao, posto_especial) VALUES (13, 'Central', 'Gestão administrativa e atendimento ao publico ', true);
 
 
 --
@@ -212,7 +206,7 @@ SELECT pg_catalog.setval('public.escala_id_seq', 3, true);
 -- Name: postos_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.postos_id_seq', 18, true);
+SELECT pg_catalog.setval('public.postos_id_seq', 20, true);
 
 
 --
@@ -260,11 +254,9 @@ ALTER TABLE ONLY public.escala
     ADD CONSTRAINT escala_postos_fk FOREIGN KEY (posto_id) REFERENCES public.postos(id);
 
 
--- Completed on 2026-02-08 19:16:17
+-- Completed on 2026-03-03 11:00:15
 
 --
 -- PostgreSQL database dump complete
 --
-
-\unrestrict TQQyCLgANUX6kpboMLle5Vcc1AlLPUtciUPhuM0Br19bBDWchVTMhdmyGDWRT6b
 
